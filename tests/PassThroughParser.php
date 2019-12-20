0f2810660f5caecc00cb673c185dfd0f41e44d02
Jon Cram <jon@webignition.net>
2019-12-20 16:04:41 +0000

    Use PHP 7.2+ only

--- tests/PassThroughParser.php
+++ tests/PassThroughParser.php
@@ -7,13 +7,12 @@ use webignition\StringParser\StringParser;
 /**
  * A simple demonstration parser that does nothing other than parse over and
  * return exactly what it has been given
- *
  */
 class PassThroughParser extends StringParser
 {
     const STATE_IN_VALUE = 1;
 
-    protected function parseCurrentCharacter()
+    protected function parseCurrentCharacter(): void
     {
         switch ($this->getCurrentState()) {
             case self::STATE_UNKNOWN:
