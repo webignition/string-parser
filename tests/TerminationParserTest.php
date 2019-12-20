05154341e2247b396aa5f81f046a4de93ce1d214
Jon Cram <webignition@gmail.com>
2019-12-20 19:08:33 +0000

    Support multibyte strings (#14)

---
+++ tests/TerminationParserTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace webignition\StringParser\Tests;
+
+class TerminationParserTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @dataProvider terminationParserDataProvider
+     */
+    public function testPassThroughParser(string $input, int $limit, string $expectedOutput)
+    {
+        $parser = new TerminationParser();
+        $parser->setLimit($limit);
+
+        $this->assertEquals($expectedOutput, $parser->parse($input));
+    }
+
+    /**
+     * @return array
+     */
+    public function terminationParserDataProvider()
+    {
+        return [
+            'non-broken ascii' => [
+                'input' => 'foo',
+                'limit' => 2,
+                'expectedOutput' => 'fo',
+            ],
+            'ascii with whitespace' => [
+                'input' => 'comes out the same as it goes in',
+                'limit' => 7,
+                'expectedOutput' => 'comes o',
+            ],
+            'utf8, cantonese, limit=9' => [
+                'input' => '我隻氣墊船裝滿晒鱔',
+                'limit' => 9,
+                'expectedOutput' => '我隻氣墊船裝滿晒鱔',
+            ],
+            'utf8, cantonese, limit=3' => [
+                'input' => '我隻氣墊船裝滿晒鱔',
+                'limit' => 3,
+                'expectedOutput' => '我隻氣',
+            ]
+        ];
+    }
+}
